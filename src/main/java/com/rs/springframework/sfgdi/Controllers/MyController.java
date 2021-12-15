150d68f461ecb080d85a168596012136e000d0f6
Ravi Shekhar <rsshekhar27@outlook.com>
2021-12-14 20:03:17 -0600

    Adding primary bean example

--- src/main/java/com/rs/springframework/sfgdi/Controllers/MyController.java
+++ src/main/java/com/rs/springframework/sfgdi/Controllers/MyController.java
@@ -1,13 +1,19 @@
 package com.rs.springframework.sfgdi.Controllers;
 
+import com.rs.springframework.sfgdi.services.GreetingService;
 import org.springframework.stereotype.Controller;
 
 
 @Controller
 public class MyController {
 
+    private final GreetingService greetingService;
+
+    public MyController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
     public String sayHello(){
-        System.out.println("Hello Spring Context !!");
-        return "Hi All !";
+        return greetingService.sayGreetings();
     }
 }
---
+++ src/main/java/com/rs/springframework/sfgdi/services/PrimaryGreetingService.java
@@ -0,0 +1,14 @@
+package com.rs.springframework.sfgdi.services;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+@Primary
+@Service
+public class PrimaryGreetingService implements GreetingService{
+
+    @Override
+    public String sayGreetings() {
+        return "Hello World from PRIMARY Bean";
+    }
+}
